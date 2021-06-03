ae023cfeda6cab3e4f3199bc175e7f7d74f4c761
edinelson <edinelsonlima86@gmail.com>
2021-06-03 18:37:32 -0300

    adicionado Projetos

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const slide = document.querySelector('img.slideImagem')
+const link = document.querySelector('a.mySlides')
+const dots =document.querySelectorAll('.dot')
+
+/*Armazena o local dos banners para serem ultilizados nos slides*/
+let banner = [
+    {src: '../img/banners/tabela-periodica.png',
+     link: 'https://edinelsonslima.github.io/tabela-periodica.github.io/'},
+
+     {src: '../img/banners/nutricao.png',
+      link: 'https://edinelsonslima.github.io/Aparecida-Nutricionista.github.io/'},
+
+    {src: '../img/banners/cronometro.png',
+     link: 'https://edinelsonslima.github.io/cronometro.github.io/'},
+
+    {src: '../img/banners/relogio-flip.png',
+     link: 'https://edinelsonslima.github.io/clock-dark-white.github.io/'},
+], i=0
+
+/*Função para trocar de Slide automaticamente*/
+addEventListener('onchange', alternarBanners())
+
+setInterval(alternarBanners,3000)
+function alternarBanners(){
+    slide.src = banner[i].src
+    link.href = banner[i].link
+    i == (banner.length-1)?i=0:i++
+}
+
+/*Funções de seleção pelos dots*/
+dots[0].addEventListener('mouseover',()=>{
+    slide.src = banner[0].src
+    link.href = banner[0].link
+})
+dots[1].addEventListener('mouseover',()=>{
+    slide.src = banner[1].src
+    link.href = banner[1].link
+})
+dots[2].addEventListener('mouseover',()=>{
+    slide.src = banner[2].src
+    link.href = banner[2].link
+})
+dots[3].addEventListener('mouseover',()=>{
+    slide.src = banner[3].src
+    link.href = banner[3].link
+})
